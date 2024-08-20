4264bfe84402cc1de757d9a3f14c4b9a5ebb8275
Latharagav <tgpremlatha@gmail.com>
2024-08-19 22:18:41 -0500

    Exercise Import Fix

--- part1/exercises/src/App.jsx
+++ part1/exercises/src/App.jsx
@@ -3,19 +3,19 @@ import './App.css';
 import MovieList from './components/MovieList.jsx';
 import ChoresList from './components/ChoresList.jsx';
 import BookList from './components/BookList.jsx';
+import HobbyLinks from './components/HobbyLinks.jsx';
 
 function App() {
   return (
     <div className="App">
-
-        <MovieList />
         <ChoresList />
-        <HobbyLinks />
-      </div>
-   
-  
+        <div className="similarComponents">
+          <MovieList />
+          <BookList />
+          <HobbyLinks />
+        </div>
+    </div>
  );
-
 }
 
 export default App;
