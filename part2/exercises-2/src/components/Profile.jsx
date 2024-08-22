cb92cefde6e970cb819efff8e03817c2507349fd
Latharagav <tgpremlatha@gmail.com>
2024-08-21 22:39:09 -0500

    Part 2 - Exercise

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,14 +1,28 @@
 import './styles.css';
 import Button from './Button';
+import Oceans from './oceans.json';
 
 
 function Profile()
-{
+{     
+   
+   const listItem = Oceans.map(ocean =>
+      <div key={ocean.id} className={`${ocean.fishCheck === 'true' ? "isAFish" : "profile"}`} >
+         <h1>{ocean.name}</h1>
+         <img className="img" src={ocean.image} alt={ocean.name} />
+         <h2>Fun Facts</h2>
+            <ol>
+               <li>{ocean.fact1}</li>
+               <li>{ocean.fact2}</li>
+               <li>{ocean.fact3}</li>
+            </ol>
+            <Button />
+      </div>
+   );
    return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
-      </>  
+      <ul>
+      {listItem}
+      </ul> 
    );
 }
 
